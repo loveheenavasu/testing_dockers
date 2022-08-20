2024406d886a71084ce84acfd33facd30e21ff0b
lovet <rivediheenalove@gmail.com>
2022-08-20 17:23:45 +0530

    added test model

--- orders/models.py
+++ orders/models.py
@@ -38,3 +38,6 @@ class OrderItem(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
 
+class Test(models.Model):
+    test = models.CharField(max_length=100, null=True, blank=True)
+
